9acb67efc30e0b88ae33447b0377e7ee4cf68d67
tolok oleksandr <tolok.oleksandr@gmail.com>
2023-01-05 15:27:00 +0100

    Fetching data,displaying,fetching data in another component with different params, rendering conditionals

--- src/views/jobs/JobDetailsView.vue
+++ src/views/jobs/JobDetailsView.vue
@@ -1,15 +1,31 @@
 <!-- @format -->
 
 <template>
-  <div>
+  <div v-if="job">
     <h1>Job Details Pge</h1>
+    <h2>{{ job.title }}</h2>
+    <p>{{ job.details }}</p>
     <p>The job id is {{ id }}</p>
   </div>
+
+  <div v-else>Loading.....</div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      job: null,
+    };
+  },
   props: ["id"],
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+  },
+
   //   data() {
   //     return {
   //       id: this.$route.params.id,
